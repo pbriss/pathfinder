cffa1500074b31f4fa2574e990fe50a03af9c549
Pascal Brisset <pbriss@gmail.com>
2015-09-14 10:37:03 -0700

    Added fabric and crashylitics to project

--- Pathfinder/Pathfinder/AppDelegate.swift
+++ Pathfinder/Pathfinder/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 
 import UIKit
+import Fabric
+import Crashlytics
+
 
 
 @UIApplicationMain
@@ -21,7 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
-    return true
+        Fabric.with([Crashlytics.self()])
+        return true
     }
 
 
